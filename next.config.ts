76bccf54bccd761e18501abff5495ad7851a6582
gyosic <gyosic@gractor,com>
2026-01-22 16:42:58 +0900

    chore: output standalone

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: false,
+  output: "standalone",
   reactCompiler: true,
+  experimental: {
+    authInterrupts: true,
+    // ppr: "incremental",
+  },
 };
 
 export default nextConfig;
